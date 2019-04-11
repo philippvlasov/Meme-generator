04344b3783d1dd93f56eab19b903fe23a86e20dc
philippvlasov <vlasov.philipp@gmail.com>
2019-04-11 15:40:57 +0300

    Updates

---
+++ precache-manifest.ca4c1142b19b22bde40f71d07fe094f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "26626d9f4c4382cdd793",
+    "url": "/Meme-generator/static/js/runtime~main.48e91657.js"
+  },
+  {
+    "revision": "a945292c9e6d27d6b32b",
+    "url": "/Meme-generator/static/js/main.328301b0.chunk.js"
+  },
+  {
+    "revision": "8c73071f762c60d7c772",
+    "url": "/Meme-generator/static/js/2.e0dc4aad.chunk.js"
+  },
+  {
+    "revision": "a945292c9e6d27d6b32b",
+    "url": "/Meme-generator/static/css/main.a91fe606.chunk.css"
+  },
+  {
+    "revision": "8c73071f762c60d7c772",
+    "url": "/Meme-generator/static/css/2.6c556dbe.chunk.css"
+  },
+  {
+    "revision": "cd0ec1c9da57aa7c7ff051bcba681cd1",
+    "url": "/Meme-generator/index.html"
+  }
+];
